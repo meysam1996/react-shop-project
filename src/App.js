e82ce6de4ed164e67119ef5d16bebce423879916
meysam1996 <meysamna514@gmail.com>
2024-06-10 16:37:27 +0330

    import products data into app component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Products from './components/products/Products'
 import Footer from './components/footer/Footer'
+import products from "./data/products"
 
 const App = () => {
+  console.log(products);
   return (
     <div>
       <Navbar />
